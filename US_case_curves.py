f112f2b0eec393343f2136733b389a6aeb074ab3
Roberto J. Avila <avila@stsci.edu>
2020-05-30 18:19:15 -0400

    Initial commit of my COVID19 analysis scripts.

---
+++ US_case_curves.py
@@ -0,0 +1,84 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import pandas as pd
+import sys
+
+matplotlib.style.use('dark_background')
+
+
+def getdata():
+
+    filename = 'COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv'
+    a = pd.read_csv(filename,index_col='UID')
+    a.drop(columns=['iso2','iso3','code3','FIPS','Admin2','Country_Region','Lat','Long_','Combined_Key'],inplace=True)
+
+    b = a.groupby('Province_State').sum()
+    dt_index = pd.to_datetime(b.columns)
+    statesdata = b.T
+    statesdata = statesdata.reindex(dt_index)
+
+    statepops = pd.read_csv('nst-est2019-01.csv',index_col='State')
+
+    return statesdata,statepops
+
+
+def state_plot(state,data):
+
+    fig = plt.figure(figsize=(6,4))
+
+    plt.bar(data.index,data[state].diff())
+    ax = plt.gca()
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%b %d'))
+    plt.gcf().autofmt_xdate(rotation=0,ha='center')
+
+    plt.ylabel('Daily new cases')
+    plt.suptitle(state,fontsize='x-large')
+
+    plt.savefig(f'{state}_new_cases.png',bbox_index='tight')
+
+
+def grid_plot(data):
+
+    fig,axes = plt.subplots(10,5,figsize=(10,12),sharex=True)
+    plt.subplots_adjust(wspace=0.3)
+
+    stateslist = ['Alabama','Alaska','Arizona','Arkansas','California',
+                  'Colorado','Connecticut','Delaware','Florida',
+                  'Georgia','Hawaii','Idaho','Illinois','Indiana','Iowa',
+                  'Kansas','Kentucky','Louisiana','Maine','Maryland',
+                  'Massachusetts','Michigan','Minnesota','Mississippi',
+                  'Missouri','Montana','Nebraska','Nevada','New Hampshire',
+                  'New Jersey','New Mexico','New York','North Carolina',
+                  'North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania',
+                  'Rhode Island','South Carolina','South Dakota','Tennessee',
+                  'Texas','Utah','Vermont','Virginia','Washington',
+                  'West Virginia','Wisconsin','Wyoming']
+
+    dailydata = data.diff()
+
+    for i,ax in enumerate(axes.flatten()):
+
+        ax.bar(dailydata.index,dailydata[stateslist[i]])
+        ax.plot(dailydata[stateslist[i]].rolling(5,center=True,min_periods=2).mean(),
+                c='gold',lw=0.75)
+        ax.set_ylim(bottom=0)
+        
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%b %d'))
+        plt.gcf().autofmt_xdate()
+
+        ax.tick_params('both',labelsize='xx-small')
+        ax.text(0.025,0.8,stateslist[i],fontsize='small',transform=ax.transAxes)
+
+
+    plt.suptitle(f'New daily cases\n{dailydata.index[-1]:%B %d, %Y}',fontsize='large')
+    plt.savefig(f'states_new_cases.pdf',bbox_inches='tight')
+
+
+if __name__ == "__main__":
+
+    data,pops = getdata()
+
+    grid_plot(data)
+
+    #state_plot()
---
+++ global_case_curves.py
@@ -0,0 +1,114 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import pandas as pd
+import sys
+
+matplotlib.style.use('dark_background')
+
+
+def getdata():
+
+    filename = 'COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
+    a = pd.read_csv(filename)
+
+    b = a.groupby('Country/Region').sum()
+    b.drop(columns=['Lat','Long'],inplace=True)
+
+    dt_index = pd.to_datetime(b.columns)
+    countriesdata = b.T
+    countriesdata = countriesdata.reindex(dt_index)
+
+    countriepops = pd.read_csv('nst-est2019-01.csv',index_col='State')
+
+    return countriesdata,countriepops
+
+
+def country_plot(country,data):
+
+    fig = plt.figure(figsize=(6,3))
+
+    plt.bar(data.index,data[country].diff())
+    plt.plot(data[country].diff().rolling(5,center=True,min_periods=2).mean(),
+             c='gold',lw=0.75)
+    ax = plt.gca()
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%b %d'))
+    plt.gcf().autofmt_xdate(rotation=0,ha='center')
+
+    plt.ylabel('Daily new cases')
+    plt.suptitle(country,fontsize='x-large')
+
+    plt.savefig(f'{country}_new_cases.png',bbox_index='tight')
+
+
+def grid_plot(data):
+
+    fig,axes = plt.subplots(3,4,figsize=(12,6),sharex=True)
+    plt.subplots_adjust(wspace=0.3)
+
+    countrieslist = ['Brazil','Costa Rica','El Salvador','Germany','Iran','Italy','Korea, South','Mexico','Russia','Spain','Sweden','US']
+
+    dailydata = data.diff()
+
+    ax = axes.flatten()
+
+    for i in range(len(countrieslist)):
+
+        #tds_corr = tds_rawmeans.rolling(window=5,min_periods=2,center=True).mean()
+        ax[i].bar(dailydata.index,dailydata[countrieslist[i]])
+        ax[i].plot(dailydata[countrieslist[i]].rolling(5,center=True,min_periods=2).mean(),
+                   c='gold',lw=1.25)
+        ax[i].set_ylim(bottom=0)
+        
+        ax[i].xaxis.set_major_formatter(mdates.DateFormatter('%b %d'))
+        plt.gcf().autofmt_xdate()
+
+        ax[i].tick_params('both',labelsize='small')
+        ax[i].text(0.025,0.85,countrieslist[i],fontsize='medium',transform=ax[i].transAxes)
+
+
+    plt.suptitle(f'New daily cases\n{dailydata.index[-1]:%B %d, %Y}',fontsize='large')
+    plt.savefig(f'global_new_cases.pdf',bbox_inches='tight')
+
+def eu_vs_usa_plot(data):
+
+    fig,ax = plt.subplots(2,1,figsize=(6,6),sharex=True)
+    #plt.subplots_adjust(wspa)
+
+    dailydata = data.diff()
+
+
+    eulist = ['Austria','Belgium','Bulgaria','Croatia','Cyprus','Czechia','Denmark','Estonia','Finland','France','Germany','Greece','Hungary','Ireland','Italy','Latvia','Lithuania','Luxembourg','Malta','Netherlands','Poland','Portugal','Romania','Slovakia','Slovenia','Spain','Sweden']
+
+    eudf = dailydata[eulist].sum(axis=1)
+
+
+    ax[0].bar(dailydata.index,dailydata['US'])
+    ax[0].plot(dailydata['US'].rolling(5,center=True,min_periods=2).mean(),
+               c='gold',lw=1.5)
+    ax[0].set_ylim(bottom=0)
+    ax[0].text(0.05,0.8,'United States',fontsize='large',transform=ax[0].transAxes)
+
+    ax[1].bar(dailydata.index,eudf)
+    ax[1].plot(dailydata.index,eudf.rolling(5,center=True,min_periods=2).mean(),
+               c='gold',lw=1.5)
+    ax[1].set_ylim(bottom=0)
+    ax[1].text(0.05,0.8,'European Union',fontsize='large',transform=ax[1].transAxes)
+    
+    ax[1].xaxis.set_major_formatter(mdates.DateFormatter('%b %d'))
+    plt.gcf().autofmt_xdate()
+
+    ax[1].tick_params('both',labelsize='small')
+
+
+    plt.suptitle(f'New daily cases\n{dailydata.index[-1]:%B %d, %Y}',fontsize='large')
+    plt.savefig(f'EU_vs_USA.pdf',bbox_inches='tight')
+
+
+if __name__ == "__main__":
+
+    data,pops = getdata()
+    eu_vs_usa_plot(data)
+    grid_plot(data)
+
+    #state_plot()
