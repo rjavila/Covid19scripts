38c9acb78742ff9923e905037e15784193d69cb4
Jo Taylor <jotaylor@stsci.edu>
2020-06-29 17:21:21 -0400

    Streamline if/else conditions

--- get_data.py
+++ get_data.py
@@ -6,16 +6,16 @@ import time
 JHU_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series"
 
 def download_data(region, deaths=False, url=JHU_URL, outdir="data"):
-    if region in ["world", "global", "latin", "eu_vs_usa"]:
-        if deaths is True:
-            filename = "time_series_covid19_deaths_global.csv"
-        else:
-            filename = "time_series_covid19_confirmed_global.csv"
-    else:
+    if region in ["usa", "us"]:
         if deaths is True:
             filename = "time_series_covid19_deaths_US.csv"
         else:
             filename = "time_series_covid19_confirmed_US.csv"
+    else:
+        if deaths is True:
+            filename = "time_series_covid19_deaths_global.csv"
+        else:
+            filename = "time_series_covid19_confirmed_global.csv"
    
     if not os.path.exists(outdir):
         os.mkdir(outdir)
@@ -36,12 +36,7 @@ def download_data(region, deaths=False, url=JHU_URL, outdir="data"):
     return outfilename
 
 def read_data(filename, region):
-    if region in ["world", "global", "latin", "eu_vs_usa"]:
-        a = pd.read_csv(filename)
-        b = a.groupby('Country/Region').sum()
-        b.drop(columns=['Lat','Long'], inplace=True)
-        statepops = None
-    else:
+    if region in ["usa", "us"]:
         a = pd.read_csv(filename, index_col='UID')
         a.drop(columns=['iso2', 'iso3', 'code3', 'FIPS', 'Admin2', 
                     'Country_Region','Lat','Long_','Combined_Key'], 
@@ -49,6 +44,11 @@ def read_data(filename, region):
         b = a.groupby('Province_State').sum()
         statepops0 = pd.read_csv('nst-est2019-01.csv',index_col='State')
         statepops = statepops0.T
+    else:
+        a = pd.read_csv(filename)
+        b = a.groupby('Country/Region').sum()
+        b.drop(columns=['Lat','Long'], inplace=True)
+        statepops = None
     try:
         b.drop(columns="Population", inplace=True)
     except KeyError:
