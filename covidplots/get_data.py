ca39139fa9666be5b848e001433f24511f869e0a
Jo Taylor <jotaylor@stsci.edu>
2021-03-31 11:43:54 -0400

    Add function to get vaccine data, format it into pandas DF, and split DF into partially and fully vaccinated data groups. Update get_data func to accept vax argument

--- covidplots/get_data.py
+++ covidplots/get_data.py
@@ -3,9 +3,45 @@ import wget
 import os
 import time
 
-from covidplots.continents import fix_jhu_df, fix_census_df
+from covidplots.continents import fix_jhu_df, fix_census_df, fix_owid_df
 
 JHU_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series"
+OWID_URL = "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations"
+
+def download_vaccine_data(region, url=OWID_URL, outdir="covid_data"):
+    """
+    Download CSV file from Our World In Data.
+    Args:
+        region (str): Country of interest. Acceptable values are 'world', 
+            'usa', 'latin', 'eu_vs_usa', 'worst_usa', 'worst_global'.
+        url (str): URL of OWID repository+directory that houses CSV files.
+        outdir (str): Name of directory to download data to.
+    Returns:
+        outfilename (str): Path of downloaded file.
+    """
+
+    if region in ["usa", "us", "worst_usa"]:
+        filename = "us_state_vaccinations.csv"
+    else:
+        filename = "vaccinations.csv"
+   
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
+    
+    outfilename = os.path.join(outdir, filename)
+    if os.path.exists(outfilename):
+        filetime = os.path.getmtime(outfilename)
+        now = time.time()
+        if now - filetime <= 43200: #43200s = 12 hours
+            print(f"File {outfilename} already up to date")
+            return outfilename
+        else:
+            os.remove(outfilename)
+
+    wget.download(os.path.join(url, filename), outfilename)
+    print(f"\n ⬇️  Downloaded {outfilename}")
+
+    return outfilename
 
 def download_data(region, deaths=False, url=JHU_URL, outdir="covid_data"):
     """
@@ -48,6 +84,73 @@ def download_data(region, deaths=False, url=JHU_URL, outdir="covid_data"):
     print(f"\n ⬇️  Downloaded {outfilename}")
 
     return outfilename
+
+def read_vaccine_data(filename, region):
+    """
+    Read data from OWID CSV files and format into a pandas DataFrame.
+    Global populations from here:
+    https://www.census.gov/data-tools/demo/idb/region.php?T=6&RT=0&A=separate&Y=2020&C=&R=1
+    Args:
+        filename (str): Path of downloaded CSV file.
+        region (str): Country of interest. Acceptable values are 'world', 
+            'usa', 'latin', 'eu_vs_usa', 'worst_usa', 'worst_global'.
+    Returns:
+        data (:obj:`pandas.DataFrame`): Vaccine statistics on region of interest.
+        pops (:obj:`pandas.DataFrame`): Population statistics on region of interst.
+    """
+    if region in ["usa", "us", "worst_usa"]:
+        data = pd.read_csv(filename)
+        data = fix_owid_df(data, world=False)
+        data.drop(data.loc[data["location"] == "United States"].index, inplace=True)
+        pops0 = pd.read_csv('geo_pop_data/nst-est2019-01.csv',index_col='State')
+        pops = pops0.T
+    else:
+        data = pd.read_csv(filename)
+        data = fix_owid_df(data)
+        regions = ["Africa", "Asia", "European Union", "Europe", "North America", "Oceania", "South America", "World", "United Kingdom"]
+        data.drop(data.loc[data["location"].isin(regions)].index, inplace=True)
+        pops0 = pd.read_csv("geo_pop_data/Census_data_2020_world_regions.csv", skiprows=1)
+        pops0.drop_duplicates(subset="Country", inplace=True) 
+        pops0.drop(columns=['Region', 'Year', 'Area (sq. km.)',
+               'Density (persons per sq. km.)'], inplace=True)
+        pops0.set_index("Country", inplace=True)
+        pops = pops0.T
+        pops = fix_census_df(pops)
+
+    data["date"] = pd.to_datetime(data["date"])
+
+    return data, pops
+
+def vax_by_region(data):
+    """
+    Convert the OWID vaccination dataframe so that each column is a country/state,
+    and the index is the date. Create a DF for number of people fully vaccinated
+    and number of people partially vaccinated.
+
+    Args:
+        data (:obj:`pandas.DataFrame`): Vaccine statistics.
+    Returns:
+        partial (:obj:`pandas.DataFrame`): Number of partially vaccinated people
+            for each country/state.
+        fully (:obj:`pandas.DataFrame`): Number of fully vaccinated people for 
+            each country/state.
+    """
+    
+    partial = data[['date', 'location', 'people_vaccinated']].copy()
+    fully = data[['date', 'location', 'people_fully_vaccinated']].copy()
+    fully = fully.pivot(index='date', 
+        columns=[x for x in fully.columns if x not in ['date', 'people_fully_vaccinated']],
+        values='people_fully_vaccinated').reset_index()
+    partial = partial.pivot(index='date', 
+        columns=[x for x in partial.columns if x not in ['date', 'people_vaccinated']],
+        values='people_vaccinated').reset_index()
+    partial.set_index('date', inplace=True)
+    fully.set_index('date', inplace=True)
+    partial = partial.fillna(method='ffill')
+    partial = partial.fillna(0)
+    fully = fully.fillna(method='ffill')
+    fully = fully.fillna(0)
+    return partial, fully
 
 def read_data(filename, region):
     """
@@ -96,7 +199,7 @@ def read_data(filename, region):
 
     return data, pops
 
-def get_data(region, deaths=False):
+def get_data(region, deaths=False, vax=False):
     """
     Convenience function to download and read JHU CSV files.
     Args:
@@ -108,6 +211,10 @@ def get_data(region, deaths=False):
         pops (:obj:`pandas.DataFrame`): Population statistics on region of interst.
     """
     
-    filename = download_data(region, deaths=deaths)
-    data, pops = read_data(filename, region)
+    if vax is True:
+        filename = download_vaccine_data(region)
+        data, pops = read_vaccine_data(filename, region)
+    else:
+        filename = download_data(region, deaths=deaths)
+        data, pops = read_data(filename, region)
     return data, pops
